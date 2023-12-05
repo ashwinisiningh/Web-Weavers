1cac2027d4c4ba1eca12af5854595c941ffca602
ashwinisingh <ashwinisingh17092005@gmail.com>
2023-12-05 01:00:16 -0800

    Added Javascript file used in linking from homepage

---
+++ 1.js
@@ -0,0 +1,33 @@
+ function refer1()
+{
+    location.href="history (4).html";
+}
+function refer2()
+{
+    location.href="About_Us";
+}
+function refer3()
+{
+    location.href="cotact_us";
+}
+function refer4()
+{
+    location.href="login_signup";
+}
+let t = document.getElementById("chg1");
+
+t.addEventListener("mouse Hover", refer2());
+
+function refer2() {
+  
+const targetElement = document.getElementById("sec");
+const hiddenImage = document.getElementById("img");
+
+targetElement.addEventListener("mouseover", () => {
+  hiddenImage.style.display = "block"; // Show the image on hover
+});
+
+targetElement.addEventListener("mouseout", () => {
+  hiddenImage.style.display = "none"; // Hide the image on mouseout
+});
+
